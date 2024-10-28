83847d76c4e63231862c173e6431e8e20a3032e7
ellandis <elamarlandis@gmail.com>
2024-10-28 14:56:34 -0400

    add usestate button

--- React/learn-state/src/App.js
+++ React/learn-state/src/App.js
@@ -1,31 +1,42 @@
 import "./index.css";
-import { useState  } from "react";
+import { useState } from "react";
 
 const messages = ["Hello React", "Hello Aria", "OOO Hello Bria"];
 
 function App() {
-  const [step,setStep] = useState(0);
+  const [step, setStep] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePre = () => {
     step > 0 ? setStep(step - 1) : setStep(step);
-
   };
   const handleNext = () => {
     step < 2 ? setStep(step + 1) : setStep(step);
   };
 
   return (
-    <div className="wrapper">
-      <div className="numberWrapper">
-        <div className={step >= 0 ? "active" : ""}>1</div>
-        <div className={step >= 1 ? "active" : ""}>2</div>
-        <div className={step >= 2 ? "active" : ""}>3</div>
-      </div>
-      <p>{step}:{messages[step]}</p>
-      <div className="btnGroup">
-        <button onClick={()=>handlePre()}>Previous</button>
-        <button onClick={()=>handleNext()}>Next</button>
-      </div>
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        
+        {isOpen ? "Hide" : "Show"}
+        
+      </button>
+      {isOpen && (
+        <div className="wrapper">
+          <div className="numberWrapper">
+            <div className={step >= 0 ? "active" : ""}>1</div>
+            <div className={step >= 1 ? "active" : ""}>2</div>
+            <div className={step >= 2 ? "active" : ""}>3</div>
+          </div>
+          <p>
+            {step}:{messages[step]}
+          </p>
+          <div className="btnGroup">
+            <button onClick={() => handlePre()}>Previous</button>
+            <button onClick={() => handleNext()}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
