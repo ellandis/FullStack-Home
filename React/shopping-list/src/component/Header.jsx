6bdcf46ae56dd718e4f4c847e2728412145cbca6
ellandis <elamarlandis@gmail.com>
2024-10-28 20:43:43 -0400

    React Shopping LIst Setup

---
+++ React/shopping-list/src/component/Header.jsx
@@ -0,0 +1,12 @@
+const Header =()=>{
+    return(
+        <div>
+            <span>💕🎶😁😍❤️🤣😂</span>
+            <h1>Shopping List</h1>
+            <span>🤩😶‍🌫️🙄😏😫😴😔😓</span>
+
+        </div>
+    )
+};
+
+export default Header;
