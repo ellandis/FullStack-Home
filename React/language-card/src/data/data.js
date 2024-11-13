4421de9a08a4a6c457e043189b48f210e693d2e9
ellandis <elamarlandis@gmail.com>
2024-11-13 18:42:19 -0500

    Language Card Application start up

---
+++ React/language-card/src/data/data.js
@@ -0,0 +1,53 @@
+import angular from "../assets/angular.png";
+import cpp from "../assets/cpp.png";
+import css from "../assets/CSS3.png";
+import html from "../assets/HTML5.png";
+import java from "../assets/java.png";
+import javascript from "../assets/javascript.png";
+import python from "../assets/python.png";
+import react from "../assets/React.png";
+
+
+export const data = [
+    {
+    name: "CSS", 
+    img: css,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "Angular", 
+    img: angular,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "Cpp", 
+    img: cpp,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "Html", 
+    img: html,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "Java", 
+    img: java,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "React", 
+    img: react,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "Python", 
+    img: python,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+    {
+    name: "JavaScript", 
+    img: javascript,
+    options: ["Option 1", "Option 2", "Option 3"]
+    },
+
+];
