8e0ac9e68a50f8cb267c494b2f1061af4408ef85
ellandis <elamarlandis@gmail.com>
2024-11-25 12:43:02 -0500

    login page setup

--- React/ell-recipe/src/pages/login/Login.jsx
+++ React/ell-recipe/src/pages/login/Login.jsx
@@ -1,11 +1,47 @@
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(-1);
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser("");
+    setEmail("");
+  };
+
+  return (
+    <div className="loginWrapper">
+      <div className="formWrapper">
+        <form action="" onSubmit={handleSubmit}>
+          <div className="inputWrapper">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="name"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </div>
+          <div className="inputWrapper">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
