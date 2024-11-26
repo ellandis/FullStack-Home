a50be9181eaa01dc425c76658c7cc1e433867f1c
ellandis <elamarlandis@gmail.com>
2024-11-26 11:26:00 -0500

    Navbar setup

--- React/ell-recipe/src/components/navbar/Navbar.jsx
+++ React/ell-recipe/src/components/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./Navbar.css";
+import {Link,NavLink  } from "react-router-dom";
 
 const Navbar = () => {
+  const [show,setShow] = useState(true);
+
+  const toggleMenu = ()=>{
+    setShow(!show);
+  }
   return (
-    <div>
-      navbar
-    </div>
+    <nav className="navWrapper">
+      <div>
+        <Link to='/'>
+        <i>{"<ELL/>"}</i>
+        <span>Recipe</span>
+        </Link>
+      </div>
+      <div className={show ? "linkWrapper show" : "linkWrapper"}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+        <NavLink to="login">Login</NavLink>
+      </div>
+      <div className="burgerMenu" onClick={toggleMenu}>
+        &#9776;
+      </div>
+    </nav>
   )
 }
 
